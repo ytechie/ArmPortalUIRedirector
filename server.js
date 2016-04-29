66102e4c93b419f7ae6074e924773d910eebbd7f
Jason Young <jason@ytechie.com>
2016-04-29 13:06:27 -0500

    Added support for POST for big datasets

--- server.js
+++ server.js
@@ -12,18 +12,23 @@ http.createServer(function(req, res) {
 		//Sample URL:
 		//http://localhost:1337/?json={}&redir=http://final.com/{jsonUrl}
 		
-		var key = Math.round(Math.random() * 100000000);
-		cache[key] = params.json;
+		var json = '';
 		
-		var retrievalUrl = 'http://armportaluiredirector.azurewebsites.net/?retrieve=' + key;
-		retrievalUrl = encodeURIComponent(retrievalUrl);
-		
-		var redir = params.redir;
-		redir = redir.replace('{jsonUrl}', retrievalUrl);
-		
-		res.statusCode = 302;
-  		res.setHeader('Location', redir);
-  		res.end();
+		if(req.method === 'POST') {
+			req.on('data', function (chunk) {
+				json += chunk;
+			});
+			
+			req.on('end', function() {
+				console.log('Received JSON via POST: ' +  json);
+				handleStoreRequest(params, json, res);
+			});
+		} else {
+			json = params.json;
+			
+			console.log('Received JSON via GET: ' +  json);
+			handleStoreRequest(params, json, res);
+		}
 	}
 	if(params['retrieve']) {
 		var key = params.retrieve;
@@ -32,3 +37,18 @@ http.createServer(function(req, res) {
 		delete cache[key];
 	}
 }).listen(process.env.PORT || 1337);
+
+function handleStoreRequest(params, json, res) {
+	var key = Math.round(Math.random() * 100000000);
+	cache[key] = json;
+	
+	var retrievalUrl = 'http://armportaluiredirector.azurewebsites.net/?retrieve=' + key;
+	retrievalUrl = encodeURIComponent(retrievalUrl);
+	
+	var redir = params.redir;
+	redir = redir.replace('{jsonUrl}', retrievalUrl);
+	
+	res.statusCode = 302;
+	res.setHeader('Location', redir);
+	res.end();
+}
